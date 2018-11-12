9970fd3ae03a038579d67da654654781d3965480
Chris Kahn <chris.kahn@ynap.com>
2018-11-12 10:48:16 +0000

    Updated doom config

--- config.el
+++ config.el
@@ -6,6 +6,9 @@
 (add-to-list 'default-frame-alist
              '(ns-transparent-titlebar . t))
 
+(setq auto-mode-alist (append '(("\\.rest$" . restclient-mode))
+      auto-mode-alist))
+
 ;; Global configuration before packages are loaded
 (setq avy-background nil
       ns-use-proxy-icon nil ;; hide window title icon
@@ -51,6 +54,63 @@
         (:desc "Save"                    :nvm "s" #'with-editor-finish
          :desc "Cancel"                  :nvm "c" #'with-editor-cancel)))
 
+;; taken from spacemacs & adjusted for my own needs
+(defun kankurisu/ediff-dotfile-and-template ()
+  "ediff the current `dotfile' with the template"
+  (interactive)
+  (ediff-files
+   "~/.doom.d/init.el"
+   "~/.emacs.d/init.example.el"))
+
+(def-package! lsp-mode
+  :after (:any cc-mode
+               c-mode
+               c++-mode
+               objc-mode
+               python
+               haskell-mode
+               rust-mode
+               caml-mode
+               vue-mode
+               rjsx-mode
+               js2-mode
+               web-mode
+               css-mode
+               scss-mode
+               sass-mode
+               less-mode
+               stylus-mode)
+  :config
+  (setq lsp-enable-eldoc t
+        lsp-eldoc-render-all t
+        lsp-hover-text-function 'lsp--text-document-signature-help
+        lsp-enable-completion-at-point t))
+
+(def-package! lsp-ui
+  :after lsp-mode
+  :config
+  (setq lsp-ui-sideline-enable nil))
+
+;; (def-package! lsp-typescript
+;;   :after lsp-mode
+;;   :config
+;;   (add-hook 'js-mode-hook #'lsp-typescript-enable)
+;;   (add-hook 'js2-mode-hook #'lsp-typescript-enable) ;; for js2-mode support
+;;   (add-hook 'rjsx-mode #'lsp-typescript-enable))
+
+(def-package! lsp-javascript-typescript
+  :after lsp-mode
+  :config
+  (add-hook 'js-mode-hook #'lsp-javascript-typescript-enable)
+  (add-hook 'typescript-mode-hook #'lsp-javascript-typescript-enable) ;; for typescript support
+  (add-hook 'js2-mode-hook #'lsp-javascript-typescript-enable) ;; for js3-mode support
+  (add-hook 'js3-mode-hook #'lsp-javascript-typescript-enable) ;; for js3-mode support
+  (add-hook 'rjsx-mode #'lsp-javascript-typescript-enable))
+
+(def-package! company-posframe
+  :config
+  (company-posframe-mode 1))
+
 ;; Global keybindings
 (map!
   (:leader
@@ -65,6 +125,18 @@
      :desc "Jump to line"          :nvm "l" #'avy-goto-line
      :desc "Jump to window"        :nvm "p" #'ace-window
     )
+
+    (:desc "Diff private config"   :nvm "hu" #'kankurisu/ediff-dotfile-and-template)
+
+    (:prefix "c"
+     :desc "Describe at point"     :nvm "c"  #'lsp-describe-thing-at-point
+     :desc "Imenu"                 :nvm "i"  #'helm-semantic-or-imenu
+     :desc "Imenu Anywhere"        :nvm "I"  #'helm-imenu-anywhere
+     :desc "Peek references"       :nvm "pr" #'lsp-ui-peek-find-references
+     :desc "Peek definitions"      :nvm "pd" #'lsp-ui-peek-find-definitions
+     :desc "Peek references"       :nvm "pi" #'lsp-ui-peek-find-implementation
+     :desc "(Un)comment selection" :nvm "/"  #'comment-or-uncomment-region
+      )
 
     (:desc "open" :prefix "o"
      :desc "Project sidebar"       :nvm "p" #'treemacs
